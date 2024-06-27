7e7013728cac0442116970928e56838d33b3c326
Müge YİŞİL <210905034@ogrenci.karatekin.edu.tr>
2024-06-27 16:41:06 +0300

    KombinasyonHesaplayanProgram

---
+++ patikaDev/src/KombinasyonHesaplayanProgram.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class KombinasyonHesaplayanProgram {
+    public static void main(String[] args) {
+//        C(n,r) = n! / (r! * (n-r)!)
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Kombinasyonunu almaka istediginiz sayilari giriniz:");
+        System.out.print("n elemanı: ");
+        int n = scan.nextInt();
+
+        int n_faktoriyel = 1;
+        for (int i = 1; i <= n; i++) {
+            n_faktoriyel *= i;
+        }
+
+        System.out.print("r elemanı: ");
+        int r = scan.nextInt();
+
+        int r_faktoriyel = 1;
+        for (int i = 1; i <= r; i++) {
+            r_faktoriyel *= i;
+        }
+
+        int n_r_faktoriyel = 1;
+        for (int i = 1; i <= (n - r); i++) {
+            n_r_faktoriyel *= i;
+        }
+        System.out.println("C(" + n + "," + r + ")= " + (n_faktoriyel / (r_faktoriyel * n_r_faktoriyel)));
+    }
+}
